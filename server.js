f245eca8b2182b2c3ba51c48b2c2af2b60043e50
Elizabeth Tackett <emctackett@gmail.com>
2020-04-14 23:10:34 -0500

    api queries CDC data for state-specific data

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
 const port = process.env.PORT || 4000;
 
 app.all('*', (req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*");
   next();
+});
+
+app.get('/state/:state_name', (req, res) => {
+  const cdcDataDump = 'http://www.cdc.gov/coronavirus/2019-ncov/map-cases-us.json';
+
+  fetch(cdcDataDump)
+    .then(res => res.json())
+    .catch(err => {
+      res.send(err);
+    })
+    .then(data => {
+      data.data.forEach(stateData => {
+        const jurisdiction = stateData['Jurisdiction'];
+
+        if (jurisdiction &&
+            jurisdiction.toLowerCase() === req.params.state_name) {
+          res.send(stateData);
+        }
+      })
+    });
 });
 
 app.listen(port, () => {
