eb694e553cf5bbb2b279bf6fbedc1f60890440f4
Elizabeth Tackett <emctackett@gmail.com>
2020-04-15 09:05:17 -0500

    add ability to query JHU for country specific information

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
 const fetch = require('node-fetch');
+const app = express();
 const port = process.env.PORT || 4000;
+const queryString = require('query-string');
 
 app.all('*', (req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*");
@@ -24,6 +25,30 @@ app.get('/state/:state_name', (req, res) => {
         if (jurisdiction &&
             jurisdiction.toLowerCase() === state) {
           res.send(stateData);
+        }
+      })
+    });
+});
+
+app.get('/country/:country_name', (req, res) => {
+  const url = 'https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/' +
+            'rest/services/Cases_by_country_Plg_V3/FeatureServer/0/query';
+  const params = 'f=json&where=1=1&returnGeometry=False' +
+                 '&spatialRel=esriSpatialRelIntersects' +
+                 '&outFields=*&orderByFields=cum_conf desc&cacheHint=true';
+  const country = req.params.country_name.toLowerCase();
+
+  fetch(`${url}?${params}`)
+    .catch(err => res.send(err))
+    .then(res => res.json())
+    .catch(err => res.send(err))
+    .then(data => {
+      data.features.forEach(countryData => {
+        const countryName = countryData.attributes['ADM0_NAME'];
+
+        if (countryName &&
+            countryName.toLowerCase() === country) {
+          res.send(countryData.attributes);
         }
       })
     });
